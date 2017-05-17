fb587a86499f356ca48aad45956605c611e85087
Aditi Hardas <aditi.hardas@gmail.com>
2017-05-17 11:21:37 +0530

    adding code

--- Spring-boot-web-jsp/src/main/java/com/springboot/example/SpringBootWebApplication.java
+++ Spring-boot-web-jsp/src/main/java/com/springboot/example/SpringBootWebApplication.java
@@ -3,10 +3,26 @@
  */
 package com.springboot.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 /**
  * @author Admin
  *
  */
-public class SpringBootWebApplication {
+	
+	@SpringBootApplication
+	public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-}
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(SpringBootWebApplication.class);
+		}
+
+		public static void main(String[] args) throws Exception {
+			SpringApplication.run(SpringBootWebApplication.class, args);
+		}
+
+	}
